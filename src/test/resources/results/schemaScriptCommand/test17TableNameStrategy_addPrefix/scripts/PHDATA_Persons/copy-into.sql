b4fd7f57ac3fb03847fcdc14ab0525d165ece514
Anish <anish@Anish-Kumars-MacBook-Pro-2.local>
2022-06-15 18:27:22 +0530

    TOOL-696 Add fields for notification integration name for pipe and task.
    Also updated the template to include changes needed for CSV file format and error_integration for pipe and task

--- src/test/resources/results/schemaScriptCommand/test17TableNameStrategy_addPrefix/scripts/PHDATA_Persons/copy-into.sql
+++ src/test/resources/results/schemaScriptCommand/test17TableNameStrategy_addPrefix/scripts/PHDATA_Persons/copy-into.sql
@@ -6,11 +6,14 @@ USE DATABASE SANDBOX_POC1;
 USE SCHEMA EMPLOYEES;
 
 COPY INTO PHDATA_Persons (PersonID,LastName,FirstName,Address,City,Age)
-FROM ( SELECT $1:PersonID::int,
+FROM ( SELECT
+
+	$1:PersonID::int,
 $1:LastName::VARCHAR(255),
 $1:FirstName::VARCHAR(255),
 $1:Address::VARCHAR(255),
 $1:City::VARCHAR(255),
 $1:Age::VARCHAR(40)
 FROM @STREAMLINER_QUICKSTART_1_stage/Persons)
-FILE_FORMAT = ( TYPE = PARQUET);
+
+	FILE_FORMAT = ( TYPE = PARQUET);
